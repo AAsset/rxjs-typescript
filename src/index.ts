78f236a906d4ca951df673cd5aea2a1e4836f15c
Asset <asset.aktay@gmail.com>
2020-02-23 14:48:02 +0600

    change input text and fetching data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { interval, of, queueScheduler, defer, timer, SchedulerLike } from 'rxjs';
-import { delay, switchMap, tap, flatMap, map, repeat, delayWhen, concatMap, take } from 'rxjs/operators';
+import { interval, of, queueScheduler, defer, timer, SchedulerLike, fromEvent } from 'rxjs';
+import { delay, switchMap, tap, flatMap, map, repeat, delayWhen, concatMap, take, debounceTime } from 'rxjs/operators';
 
 // v^v delay
 export const createSpecialInterval = (number: number, scheduler ?: SchedulerLike ) => {
@@ -8,6 +8,47 @@ export const createSpecialInterval = (number: number, scheduler ?: SchedulerLike
 
 createSpecialInterval(100).subscribe(v => {
   console.log(v);
-  console.timeEnd('interval');
-  console.time('interval');
+  // console.timeEnd('interval');
+  // console.time('interval');
 });
+
+const myInput = document.querySelector('input');
+
+// const p = new Promise((resolve) => {
+//   myInput.addEventListener('input', (event) => {
+//     resolve(event);
+//   });
+// });
+
+// p.then((event: KeyboardEvent) => console.log((event.target as HTMLInputElement).value));
+
+// function createEventPromise() {
+//   let listener: (event: KeyboardEvent) => void;
+//   const p = new Promise((resolve) => {
+//     listener = (event) => {
+//       resolve(event);
+//     }
+//     myInput.addEventListener('input', listener);
+//   });
+
+//   p.then((event: KeyboardEvent) => {
+//     console.log((event.target as HTMLInputElement).value);
+//     fetch(`https://api.github.com/search/repositories?q=${(event.target as HTMLInputElement).value}`)
+//     .then(response => response.json())
+//     .then((response) => {
+//       console.log('Repositories count: ', response.total_count);
+//     });
+//     myInput.removeEventListener('input', listener);
+//     createEventPromise();
+//   });
+// }
+// createEventPromise();
+
+const observable = fromEvent(myInput, 'input');
+observable.pipe(
+  debounceTime(500),
+  switchMap((event: KeyboardEvent) => {
+    return fetch(`https://api.github.com/search/repositories?q=${(event.target as HTMLInputElement).value}`)
+           .then((response => response.json()));
+  })
+).subscribe((response: any) => console.log('Repositories count: ', response.total_count));
